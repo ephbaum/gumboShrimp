9a23de22f5da8923a5a3764fa0c686ab2da2edac
Heriberto Miguel <herimiguel84@hotmail.com>
2020-05-16 16:39:00 -0500

    item component added

---
+++ resources/js/components/ItemComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h2>itemComponent
+            </h2>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
