4dbe4a599fe6e78b47754c2c5be0552b8ce29d6d
Bald Mike <baldmike312@gmail.com>
2020-05-19 23:23:51 -0500

    quick fix

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -19,21 +19,11 @@ class SpaController extends Controller
 
     public function index()
     {
-<<<<<<< HEAD
-        Log::debug('SpaController->index()');
-        return view('spa');
-=======
         return view('home');
->>>>>>> c7135a1546a4ae7f76375929967a618f1dd17318
     }
 
     public function notFound()
     {
-<<<<<<< HEAD
-        Log::debug('SpaController->notFound()');
-        return view('spa');
-=======
         return view('home');
->>>>>>> c7135a1546a4ae7f76375929967a618f1dd17318
     }
 }
