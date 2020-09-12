5a2589bb17c8350633e0347ee4000188cc87bb1a
Bald Mike <baldmike312@gmail.com>
2020-09-12 15:48:51 -0500

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Laravel\Passport\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasApiTokens;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -27,5 +27,14 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
