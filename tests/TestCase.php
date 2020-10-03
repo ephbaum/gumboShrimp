e37e198d745972df7399f2f0813ccd4062dcf5c4
Bald Mike <baldmike312@gmail.com>
2020-10-03 17:14:57 -0500

    adds login user to base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,32 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function loginRandomAdmin()
+    {
+        $user = User::inRandomOrder()->where('role', 'admin')->first();       
+
+        $tokenData = [
+            'grant_type' => 'password',
+            'email' => trim($user->email),
+            'password' => 'password',
+            'scope' => ''
+        ];
+
+        $response = $this->json('POST', '/api/login', $tokenData);
+
+        $json = json_decode($response->content());
+
+        if($response->getStatusCode() == 200)
+        {
+            $this->token = $json->token;
+        }
+
+        return $response;
+    }
 }
