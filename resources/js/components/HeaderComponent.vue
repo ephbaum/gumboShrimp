1509252bf85417040fc0e5c358134c5b6a06d618
Bald Mike <baldmike312@gmail.com>
2021-01-03 13:00:24 -0600

    new header

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <b-container fluid>
-        <b-img class="headImage col-md-12" fluid src="../images/fruits.jpeg" alt="Image 1"></b-img>
+        <!-- <b-img class="headImage col-md-12" fluid src="../images/fruits.jpeg" alt="Image 1"></b-img> -->
+        <div class="banner"><p>Chango Cart</p></div>
+        <div class="subtitle">A place to buy stuff.</div>
     </b-container>
 </template>
 
@@ -35,8 +37,18 @@ export default {
 </script>
 
 <style>
-.headImage{
+.headImage {
     max-height: 375px;
     margin-top: 60px;
+}
+
+.banner {
+    text-align: center;
+    font-size: 8rem;
+}
+
+.subtitle {
+    text-align: center;
+    font-size: 2rem;
 }
 </style>
