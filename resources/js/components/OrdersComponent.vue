4d3859ee30720ffb3502b155e14e21941f0400e4
Bald Mike <baldmike312@gmail.com>
2021-02-14 09:21:36 -0600

    clean up OrdersComponent

--- resources/js/components/OrdersComponent.vue
+++ resources/js/components/OrdersComponent.vue
@@ -1,71 +1,65 @@
 <template>
- <b-container fluid class="container-box">
-<b-row id="card-body">
-            <b-col lg="3" md="4" sm="6" v-for="order in orders" :key="order.id">
-                <b-card class="fadeIn">
-                    
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> Order Id: {{ order.id }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Name: {{ order.name }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Email: {{ order.email }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> Address: {{ order.address }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> City: {{ order.city }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em"> State: {{ order.state }} </b-card-text>
-                        </b-col>
-                    </b-row>
-                    <b-row class="text-center">
-                        <b-col>
-                            <b-card-text style="font-size: .7em">Zip: {{ order.zip }} </b-card-text>
-                        </b-col>
-                    </b-row>
+<b-container fluid class="container-box">
+    <b-row id="card-body">
+        <b-col lg="3" md="4" sm="6" v-for="order in orders" :key="order.id">
+            <b-card class="fadeIn">
+                
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em"> Order Id: {{ order.id }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em">Name: {{ order.name }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em">Email: {{ order.email }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em"> Address: {{ order.address }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em"> City: {{ order.city }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em"> State: {{ order.state }} </b-card-text>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center">
+                    <b-col>
+                        <b-card-text style="font-size: .7em">Zip: {{ order.zip }} </b-card-text>
+                    </b-col>
+                </b-row>
 
-                    <b-row v-for='(item, id) in order.order_item' :key="id" class="text-center">
-                        <b-col>
+                <b-row v-for='(item, id) in order.order_item' :key="id" class="text-center">
+                    <b-col>
+                        {{ item["quantity"] }} -- {{ item["item_name"] }}
+                    </b-col>
+                </b-row>
 
-                            {{ item["quantity"] }} -- {{ item["item_name"] }}
-                        
-                        </b-col>
-                    </b-row>
+                <b-row class="text-center bottom">
+                    <b-col>
+                        <b-card-text>amount: {{ order.amount }} </b-card-text>
+                    </b-col>
+                </b-row>
 
-                    <b-row class="text-center bottom">
-                        <b-col>
-
-                            <b-card-text>amount: {{ order.amount }} </b-card-text>
-                            
-                        </b-col>
-                    </b-row>
-
-                    <b-row class="text-center bottom">
-                        <b-col>
-
-                            <b-button @click.prevent="fulfillOrder(order.id)">Fulfill</b-button>
-                        
-                        </b-col>
-                    </b-row>
-                </b-card>
-            </b-col>
-        </b-row>
+                <b-row class="text-center bottom">
+                    <b-col>
+                        <b-button @click.prevent="fulfillOrder(order.id)">Fulfill</b-button>
+                    </b-col>
+                </b-row>
+            </b-card>
+        </b-col>
+    </b-row>
 
 </b-container>
     
