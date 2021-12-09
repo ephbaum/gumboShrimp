ff2e5f49cf29621b1d0986a794c561996f95f4a4
Bald Mike <baldmike312@gmail.com>
2021-12-09 17:49:45 -0600

    change name

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <meta name="csrf-token" content="{{ csrf_token()}}"> 
     
-        <title>CB Store</title>
+        <title>Chango Cart</title>
 
         <!-- Fonts -->
         <link href="https://bootswatch.com/4/litera/bootstrap.min.css" rel="stylesheet">
