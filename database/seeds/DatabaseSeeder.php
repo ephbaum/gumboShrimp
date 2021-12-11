35ae92cf6ee64739bcc119ff78f857d62750f498
Bald Mike <baldmike312@gmail.com>
2021-12-11 17:27:00 -0600

    adds admin seeder to db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ItemSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            AdminSeeder::class
         ]);
     }
 }
