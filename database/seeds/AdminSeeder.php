7b99f8e524ef7dba449e905f549769920f87a18c
Bald Mike <baldmike312@gmail.com>
2021-12-11 17:27:12 -0600

    adds admin seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+  
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+   
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'password' =>  bcrypt('password'),
+            'remember_token' => Str::random(10)
+        ]);
+    }
+}
